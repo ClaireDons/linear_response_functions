30c75b658fc236409e8575ec8ee5ee28d63ee43c
“Claire <claire.donnelly@knmi.nl>
2023-05-10 21:00:18 +0000

    Reformatted directory and added gitignore

--- calculate_LRF.py
+++ calculate_LRF.py
@@ -1,9 +1,10 @@
 import os
+import pandas as pd
 from glob import glob
-from amrstats_functions import *
-from LRF_functions import *
+from lrf_calc import amrstats_functions as amrstats
+from lrf_calc import lrf_functions as lrf
 
-path = r'/nobackup/users/donnelly/Antarctica/LRF8'
+path = r'/nobackup/users/donnelly/projects/LinearResponseFunctions/data'
 files = glob(os.path.join(path, "plot.*"))
 
 masks_path = r'/nobackup/users/donnelly/levermann-masks'
@@ -11,7 +12,7 @@ mask_files = glob(os.path.join(masks_path, "*.2d.hdf5"))
 
 filetoolsPath = '/usr/people/donnelly/bisicles/BISICLES/code/filetools/'
 filetoolStats = 'stats2d.Linux.64.g++.gfortran.DEBUG.ex'
-csv_path = r'/nobackup/users/donnelly/LRF_csv/'
+csv_path = r'/nobackup/users/donnelly/projects/LinearResponseFunctions/lrf_csv/'
 experiment = "LRF8"
 bm = 8.0
 
@@ -22,15 +23,14 @@ for mask in mask_files:
     if os.path.isfile(name) == True:
         df = pd.read_csv(name)
         if 'SMA10' not in df.columns:
-            df = LRF_calc(df,bm)
+            df = lrf.LRF_calc(df,bm)
             df.to_csv(name,index = False)
         elif 'SMA10' in df.columns:
-            #df = df.drop(columns=['ice_mass','SLE','SLEm','difference','LRF','SMA10'])
-            df = LRF_calc(df,bm)
+            df = lrf.LRF_calc(df,bm)
             df.to_csv(name,index = False)
         else:
             print("Something odd happened")
     else:
-        df = AMRplot_DF(filetoolsPath,filetoolStats,files,mask)
-        df = LRF_calc(df,bm)
+        df = amrstats.AMRplot_DF(filetoolsPath,filetoolStats,files,mask)
+        df = lrf.LRF_calc(df,bm)
         df.to_csv(name,index = False)
