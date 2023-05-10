a18b24ecc870b19efc6abfe24112f89845184484
“Claire <claire.donnelly@knmi.nl>
2023-05-10 21:21:14 +0000

    reformatted files

--- calculate_LRF.py
+++ calculate_LRF.py
@@ -4,33 +4,40 @@ from glob import glob
 from lrf_calc import amrstats_functions as amrstats
 from lrf_calc import lrf_functions as lrf
 
-path = r'/nobackup/users/donnelly/projects/LinearResponseFunctions/data'
-files = glob(os.path.join(path, "plot.*"))
+path = r"/nobackup/users/donnelly/projects/LinearResponseFunctions/data"
+masks_path = r"/nobackup/users/donnelly/levermann-masks"
+csv_path = r"/nobackup/users/donnelly/projects/LinearResponseFunctions/lrf_csv/"
 
-masks_path = r'/nobackup/users/donnelly/levermann-masks'
-mask_files = glob(os.path.join(masks_path, "*.2d.hdf5"))
+filetoolsPath = "/usr/people/donnelly/bisicles/BISICLES/code/filetools/"
+filetoolStats = "stats2d.Linux.64.g++.gfortran.DEBUG.ex"
 
-filetoolsPath = '/usr/people/donnelly/bisicles/BISICLES/code/filetools/'
-filetoolStats = 'stats2d.Linux.64.g++.gfortran.DEBUG.ex'
-csv_path = r'/nobackup/users/donnelly/projects/LinearResponseFunctions/lrf_csv/'
 experiment = "LRF8"
-bm = 8.0
+basal_melt = 8.0
 
-for mask in mask_files:
-    key = os.path.splitext(os.path.basename(mask))[0][14:-3]
-    name = csv_path + experiment + key + ".csv"
-    print(name)
-    if os.path.isfile(name) == True:
-        df = pd.read_csv(name)
-        if 'SMA10' not in df.columns:
-            df = lrf.LRF_calc(df,bm)
-            df.to_csv(name,index = False)
-        elif 'SMA10' in df.columns:
-            df = lrf.LRF_calc(df,bm)
-            df.to_csv(name,index = False)
+
+def main(plot_files, masks):
+    for mask in masks:
+        key = os.path.splitext(os.path.basename(mask))[0][14:-3]
+        name = csv_path + experiment + key + ".csv"
+        print(name)
+        if os.path.isfile(name) == True:
+            df = pd.read_csv(name)
+            if "SMA10" not in df.columns:
+                df = lrf.lrf_calc(df, basal_melt)
+                df.to_csv(name, index=False)
+            elif "SMA10" in df.columns:
+                df = lrf.lrf_calc(df, basal_melt)
+                df.to_csv(name, index=False)
+            else:
+                print("Something odd happened")
         else:
-            print("Something odd happened")
-    else:
-        df = amrstats.AMRplot_DF(filetoolsPath,filetoolStats,files,mask)
-        df = lrf.LRF_calc(df,bm)
-        df.to_csv(name,index = False)
+            df = amrstats.AMRplot_DF(filetoolsPath, filetoolStats, plot_files, mask)
+            df = lrf.lrf_calc(df, basal_melt)
+            df.to_csv(name, index=False)
+
+
+if __name__ == "__main__":
+    files = glob(os.path.join(path, "plot.*"))
+    mask_files = glob(os.path.join(masks_path, "*.2d.hdf5"))
+
+    main(files, mask_files)
--- lrf_calc/lrf_functions.py
+++ lrf_calc/lrf_functions.py
@@ -2,30 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def LRF_calc(df,bm):
-    '''Function to calculate a Linear Response Function from 
+def lrf_calc(df, bm):
+    """Function to calculate a Linear Response Function from
     Levermann et al. (2020) based on stats output from BISICLES
     df: dataframe containing BISICLES data
-    returns dataframe with additional columns'''
-    
+    returns dataframe with additional columns"""
+
     sea_area = 361.8
     rho_i = 0.917
-    
-    df['ice_mass'] = (df.volumeAbove/(10**9)) * rho_i
-    df['SLE'] = df.ice_mass*(1/sea_area)
-    df['SLEm'] = -(df.SLE)/1000
-    df['difference'] = df.SLEm.diff()
-    df['LRF'] = df.difference/bm
-    df['SMA10'] = df.LRF.rolling(10).mean()
+
+    df["ice_mass"] = (df.volumeAbove / (10**9)) * rho_i
+    df["SLE"] = df.ice_mass * (1 / sea_area)
+    df["SLEm"] = -(df.SLE) / 1000
+    df["difference"] = df.SLEm.diff()
+    df["LRF"] = df.difference / bm
+    df["SMA10"] = df.LRF.rolling(10).mean()
     return df
 
-def LRF_ts(df,key,plot_path):
-    '''Function to plot LRF from Levermann et al. (2020) based on
+
+def lrf_ts(df, key, plot_path):
+    """Function to plot LRF from Levermann et al. (2020) based on
     dataframe created in LRF_calc. Returns a plot with LRF and
-    10 year running mean'''
-    
-    plot = plt.plot(df['time'],df['LRF'])
-    plt.plot(df['time'],df['SMA10'])
+    10 year running mean"""
+
+    plot = plt.plot(df["time"], df["LRF"])
+    plt.plot(df["time"], df["SMA10"])
     plt.xlabel("Time (years)")
     plt.title(key)
     plt.show()
