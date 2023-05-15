fb6f1d2522f48fbaaef62194e8b827295b3ad3f8
“Claire <claire.donnelly@knmi.nl>
2023-05-15 14:23:39 +0000

    Initialised testing modules

---
+++ tests/test_amrstats_functions.py
@@ -0,0 +1,16 @@
+""" Tests for functions in the amrstats_functions module"""
+
+import pytest
+
+
+def test_run_statstool():
+    from lrf_calc.amrstats_functions import run_statstool
+
+def test_create_series():
+    from lrf_calc.amrstats_functions import create_series
+
+def test_stats_retrieve():
+    from lrf_calc.amrstats_functions import stats_retrieve
+
+def test_amrplot_df():
+    from lrf_calc.amrstats_functions import amrplot_df
